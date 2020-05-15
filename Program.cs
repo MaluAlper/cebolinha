68183ec575aef5a2bc46cc86377d69e5b52484f5
Maria Luiza <malualper@hotmail.com>
2020-05-15 14:11:08 -0300

    Cebolinha

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DIGITE UMA MENSAGEM");
+            string textodigitado = Console.ReadLine(); 
+            
+            Console.WriteLine("COMO O CEBOLINHA FALARIA:");
+            string Cebolinha = textodigitado.Replace("r", "l");
+            Console.WriteLine(Cebolinha);
+            Console.ReadLine();
+
+
+        }
+    }
+}
